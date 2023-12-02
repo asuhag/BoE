91d1c682c8f6b09034ac8f91d46b636ec3737fdf
Anuj Suhag <32389931+asuhag@users.noreply.github.com>
2023-12-02 12:26:42 +0000

    Access all data

--- explore_datafile.py
+++ explore_datafile.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-data = pd.read_excel('DataScientist_009749_Dataset.xlsx')
+data_pg1 = pd.read_excel('DataScientist_009749_Dataset.xlsx',sheet_name= 'Dataset 1 - General')
+data_pg2 = pd.read_excel('DataScientist_009749_Dataset.xlsx',sheet_name= 'Dataset 2 - Underwriting')
 
-print(data.columns)
-
-print(data.head())
+print(data_pg1.head())
+print(data_pg2.head())
