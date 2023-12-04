80bb7f8a3f073b03f51da4553ff86ea88da20741
Anuj Suhag <32389931+asuhag@users.noreply.github.com>
2023-12-04 13:39:10 +0000

    Task 1

--- data_wrangling.py
+++ data_wrangling.py
@@ -15,3 +15,54 @@ merged_df = pd.merge(left=page_1, right= page_2, left_on='Firm', right_on='Firm'
 merged_df.drop(index = 0, inplace = True)
 #merged_df.to_csv('data.csv', index = False)
 
+convert_columns_to_numeric(merged_df)
+new_df = replace_outliers_with_mad(merged_df)
+
+# Top 5 companies with high liquidity risk 
+new_df['Avg SCR cr'] = new_df.filter(regex='SCR cr').mean(axis = 1)
+top_5_lowest_scr = new_df.sort_values(by='Avg SCR cr').head(5)[['Firm','Avg SCR cr']]
+plot_top_firms_with_average(new_df, top_5_lowest_scr, 'Avg SCR cr', 'Liquidity risk - Top 5 Firms with Lowest Average SCR coverage ratio', 
+                            'Firm', 'Average SCR coverage ratio') 
+
+# Top 5 companies with high strategic risk
+new_df['Avg NBEL'] = new_df.filter(regex='NBEL').mean(axis = 1)
+top_5_highest_nbel = new_df.sort_values(by='Avg NBEL', ascending=False).head(5)[['Firm','Avg NBEL']]
+
+plot_top_firms_with_average(new_df, top_5_highest_nbel, 'Avg NBEL', 'Strategic risk - Top 5 Firms with Highest Average NBEL', 
+                            'Firm', 'Average NBEL Ratio') 
+
+# Top 5 companies with high operation risk 
+new_df['Avg NCR'] = new_df.filter(regex='NCR').mean(axis=1)
+top_5_highest_ncr = new_df.sort_values(by='Avg NCR', ascending=False).head(5)[['Firm', 'Avg NCR']]
+plot_top_firms_with_average(new_df, top_5_highest_ncr, 'Avg NCR', ' Operation risk - Top 5 Firms with Highest Average NCR', 
+                            'Firm', 'Average NCR Ratio') 
+
+# Top 5 companies with high strategic risk because of reinsurance dependence 
+years = ['2016', '2017', '2018', '2019', '2020']
+
+# Adding a small value to NWP to avoid division by zero
+small_value = 1e-5
+for year in years:
+    gwp_col = f'GWP {year}'
+    nwp_col = f'NWP {year}'
+    ratio_col = f'GWP/NWP Ratio {year}'
+
+    # Adding small value to NWP and ensuring NWP is not extremely small
+    new_df[nwp_col] = new_df[nwp_col].apply(lambda x: x + small_value if x < small_value else x)
+
+    # Calculating the ratio
+    new_df[ratio_col] = new_df[gwp_col] / new_df[nwp_col]
+
+# Replace 'inf' values with NaN
+new_df.replace([np.inf, -np.inf], np.nan, inplace=True)
+
+# Calculating the average GWP/NWP ratio across years
+new_df['Avg GWP/NWP'] = new_df.filter(regex='GWP/NWP Ratio').mean(axis=1)
+
+# Finding the top 5 firms with the highest average GWP/NWP ratio
+top_5_highest_reinsurance = new_df.sort_values(by='Avg GWP/NWP', ascending=False).head(5)[['Firm', 'Avg GWP/NWP']]
+
+print("Top 5 Firms with Highest Average GWP/NWP Ratio:")
+print(top_5_highest_reinsurance)
+plot_top_firms_with_average(new_df, top_5_highest_reinsurance, 'Avg GWP/NWP', 'Strategic risk - Top 5 Firms with Highest Dependence on Reinsurance', 
+                            'Firm', 'Average GWP/NWP Ratio') 
--- explore_datafile.py
+++ explore_datafile.py
@@ -7,9 +7,6 @@ def convert_columns_to_numeric(df, exclude_col='Firm'):
     """
     Convert all columns in the DataFrame to numeric types, except for the specified exclude column.
 
-    :param df: Pandas DataFrame
-    :param exclude_col: Column name to be excluded from conversion (default is 'Firm')
-    :return: DataFrame with converted columns
     """
     for col in df.columns:
         if col != exclude_col:
@@ -18,11 +15,13 @@ def convert_columns_to_numeric(df, exclude_col='Firm'):
 
 
 def get_data(filepath,sheetname):
+    """
+    Read data from file and sheet. Rename columns for ease of query
+
+    """
 
     current_name_1 = ['Unnamed: 0', 'NWP (£m) ', 'NWP (£m) .1', 'NWP (£m) .2', 'NWP (£m) .3',
-       'NWP (£m) .4', 'SCR (£m)', 'SCR (£m).1', 'SCR (£m).2', 'SCR (£m).3',
-       'SCR (£m).4', 'EoF for SCR (£m)', 'EoF for SCR (£m).1',
-       'EoF for SCR (£m).2', 'EoF for SCR (£m).3', 'EoF for SCR (£m).4',
+       'NWP (£m) .4',
        'SCR coverage ratio', 'SCR coverage ratio.1', 'SCR coverage ratio.2',
        'SCR coverage ratio.3', 'SCR coverage ratio.4', 'GWP (£m)',
        'GWP (£m).1', 'GWP (£m).2', 'GWP (£m).3', 'GWP (£m).4', 
@@ -33,9 +32,7 @@ def get_data(filepath,sheetname):
        'Excess of assets over liabilities (£m) [= equity].4']
     
     new_name_1 = ['Firm', 'NWP 2016', 'NWP 2017', 'NWP 2018', 'NWP 2019',
-       'NWP 2020', 'SCR 2016', 'SCR 2017', 'SCR 2018', 'SCR 2019',
-       'SCR 2020', 'EoF for SCR 2016', 'EoF for SCR 2017',
-       'EoF for SCR 2018', 'EoF for SCR 2019', 'EoF for SCR 2020',
+       'NWP 2020',
        'SCR cr 2016', 'SCR cr 2017', 'SCR cr 2018',
        'SCR cr 2019', 'SCR cr 2020', 'GWP 2016',
        'GWP 2017', 'GWP 2018', 'GWP 2019', 'GWP 2020', 
@@ -82,80 +79,19 @@ def get_data(filepath,sheetname):
         df.rename(columns=mapper_2, errors="raise", inplace=True)
         return df[new_name_2]
     
-def replace_outliers_with_median_except(df, exclude_col, threshold=3):
-    """
-    Replace outliers in all numeric columns of a DataFrame, except for a specified column, with the median of the respective column.
-
-    :param df: Pandas DataFrame
-    :param exclude_col: Column name to exclude from outlier processing
-    :param threshold: Z-score value to determine outliers (default is 3)
-    :return: inplace DataFrame with outliers replaced by median values
-    """
-    numeric_cols = [col for col in df.columns if col != exclude_col]
-
-    for col in numeric_cols:
-        col_median = df[col].median()
-        col_z_score = np.abs((df[col] - df[col].mean()) / df[col].std())
-        df.loc[col_z_score > threshold, col] = col_median
-
-    return df
-
-def replace_outliers_with_median(df, exclude_col = 'Firm', threshold=3):
-    """
-    Replace outliers in all numeric columns of a DataFrame, except for a specified column, with the median of the respective column.
-
-    :param df: Pandas DataFrame
-    :param exclude_col: Column name to exclude from outlier processing
-    :param threshold: Z-score value to determine outliers (default is 3)
-    :return: DataFrame with outliers replaced by median values
-    """
-    # Creating a copy of the DataFrame to avoid modifying the original one
-    df_copy = df.copy()
-
-    # Identifying numeric columns except the exclude_col
-    numeric_cols = [col for col in df_copy.columns if df_copy[col].dtype.kind in 'biufc' and col != exclude_col]
-
-    # Iterating through each numeric column
-    for col in numeric_cols:
-        # Calculating the median, mean, and standard deviation for the column
-        col_median = df_copy[col].median()
-        col_mean = df_copy[col].mean()
-        col_std = df_copy[col].std()
-
-        # Avoid division by zero in case of constant column
-        if col_std == 0:
-            continue
-
-        # Calculating the Z-score for each value in the column
-        col_z_score = np.abs((df_copy[col] - col_mean) / col_std)
-
-        # Identifying outliers
-        outliers = col_z_score > threshold
-
-        # Replacing outliers with the median value
-        df_copy.loc[outliers, col] = col_median
-
-    return df_copy
 
 def replace_outliers_with_mad(df, exclude_col = 'Firm', threshold=3.5):
     """
     Replace outliers in all numeric columns of a DataFrame, except for a specified column, with the median of the respective column.
     Outliers are determined based on Median Absolute Deviation (MAD).
 
-    :param df: Pandas DataFrame
-    :param exclude_col: Column name to exclude from outlier processing
-    :param threshold: Threshold value to determine outliers based on MAD (default is 3.5)
-    :return: DataFrame with outliers replaced by median values
     """
-    # Creating a copy of the DataFrame to avoid modifying the original one
     df_copy = df.copy()
 
     # Identifying numeric columns except the exclude_col
     numeric_cols = [col for col in df_copy.columns if df_copy[col].dtype.kind in 'biufc' and col != exclude_col]
 
-    # Iterating through each numeric column
     for col in numeric_cols:
-        # Calculating the median for the column
         col_median = df_copy[col].median()
 
         # Calculating the Median Absolute Deviation (MAD)
@@ -176,51 +112,20 @@ def replace_outliers_with_mad(df, exclude_col = 'Firm', threshold=3.5):
 
     return df_copy
 
-def visualize_outliers(df, cols):
+def plot_top_firms_with_average(df, top_firms, ratio_column, title, x_label, y_label):
     """
-    Visualize outliers in specified columns of a DataFrame using boxplots.
-
-    :param df: Pandas DataFrame
-    :param cols: List of column names to visualize for outliers
+    Function to plot top firms based on a specific ratio along with the overall average of that ratio.
     """
-    num_plots = len(cols)
-    plt.figure(figsize=(10, 4 * num_plots))
+    
+    # Calculate the overall average ratio
+    overall_avg_ratio = df[ratio_column].mean()
 
-    for i, col in enumerate(cols, 1):
-        plt.subplot(num_plots, 1, i)
-        sns.boxplot(x=df[col])
-        plt.title(f'Boxplot of {col}')
-
-    plt.tight_layout()
+    # Plotting
+    plt.figure(figsize=(12, 6))
+    sns.barplot(x='Firm', y=ratio_column, data=top_firms, palette='Set2')
+    plt.axhline(overall_avg_ratio, color='red', linestyle='--', label=f'Overall Average Ratio = {overall_avg_ratio:.2f}')
+    plt.title(title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.legend()
     plt.show()
-
-def year_on_year_growth(df, metric_prefix):
-    """
-    Calculate year-on-year growth for a given metric and identify top 5 firms with the highest growth.
-    Handles cases where the metric has zero entries and ensures numeric data types.
-
-    :param df: Pandas DataFrame containing the data.
-    :param metric_prefix: Prefix of the metric columns (e.g., 'SCR').
-    :return: List of top 5 firms with the highest year-on-year growth.
-    """
-    # Filter columns related to the metric
-    metric_cols = [col for col in df.columns if metric_prefix in col]
-    metric_cols.sort()  # Ensure columns are in the correct year order
-
-    # Calculate year-on-year growth
-    epsilon = 1e-5  # Small value to avoid division by zero
-    for i in range(len(metric_cols) - 1):
-        # Avoid division by zero by adding epsilon to the denominator
-        df[f'Growth {metric_cols[i+1]}'] = (df[metric_cols[i+1]] - df[metric_cols[i]]) / (df[metric_cols[i]] + epsilon)
-
-    # Calculate average growth for each firm
-    growth_cols = [col for col in df.columns if 'Growth' in col]
-    df['Average Growth'] = df[growth_cols].mean(axis=1)
-
-    # Ensure 'Average Growth' is treated as numeric
-    df['Average Growth'] = pd.to_numeric(df['Average Growth'], errors='coerce')
-
-    # Identify top 5 firms with the highest average growth
-    top_firms = df.nlargest(5, 'Average Growth')['Firm']
-
-    return top_firms.tolist()
